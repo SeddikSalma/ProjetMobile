9ed7159d8a2ff0a2e48181676e200b8a7ea0ec46
SeddikSalma <seddiksalma@gmail.com>
2024-01-22 16:46:17 +0100

    UIs

---
+++ app/src/main/java/com/example/project/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project.databinding.ActivitySplashscreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+}
