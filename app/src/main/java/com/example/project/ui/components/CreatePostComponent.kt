5f1db71d6344f5e27b94f0d29f8d43c7779ec9aa
Mohamed Mongi Saidane <saidanemongi@gmail.com>
2024-01-23 02:08:01 +0100

    Add jetpack compose

---
+++ app/src/main/java/com/example/project/ui/components/CreatePostComponent.kt
@@ -0,0 +1,67 @@
+package com.example.project.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.project.R
+
+@Composable
+@Preview
+fun CreatePostComponentPreview(){
+    return CreatePostComponent()
+}
+
+@Composable
+fun CreatePostComponent(){
+    val text = remember {
+        mutableStateOf("")
+    }
+    return Column {
+        TextField(
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+            ),
+            modifier = Modifier
+                .background(Color.White)
+                .padding(8.dp, 0.dp, 8.dp, 0.dp)
+                .fillMaxWidth(),
+            value = text.value,
+            onValueChange = { newText -> text.value = newText },
+            label = { Text(text = "What's on your mind?") },
+        )
+        Row (
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 8.dp, 0.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF4500)
+                ),
+                onClick = {
+                    // API Call to create post
+                }
+            ) {
+                Text("Post")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project/ui/components/post/PostBody.kt
@@ -0,0 +1,27 @@
+package com.example.project.ui.components.post
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(widthDp = 400, heightDp = 300)
+fun PostBodyPreview(){
+    return PostBody("Hello World\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+}
+
+@Composable
+fun PostBody(body: String){
+    return Column (
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ){
+        Text(text = body)
+    }
+}
