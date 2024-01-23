c0391fe345300e1d17cfc6ea369b1abe7a6693d7
Your Name <you@example.com>
2024-01-23 17:08:41 +0100

    ghro9na

--- app/src/main/java/com/example/project/view/SignupActivity.kt
+++ app/src/main/java/com/example/project/view/SignupActivity.kt
@@ -6,48 +6,90 @@ import android.util.Patterns
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.example.project.R
 import com.example.project.databinding.ActivitySignupBinding
+import com.example.project.repository.AuthRepository
+import com.example.project.utils.APIService
+import com.example.project.view_model.SignupActivityViewModel
+import com.example.project.view_model.SignupActivityViewModelFactory
 
 class SignupActivity : AppCompatActivity(), View.OnClickListener,View.OnFocusChangeListener,View.OnKeyListener {
 
    private lateinit var binding : ActivitySignupBinding
+   private lateinit var viewModel : SignupActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivitySignupBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        binding.fullName.onFocusChangeListener=this
-        binding.editTextEmail.onFocusChangeListener=this
-        binding.editTextPassword.onFocusChangeListener=this
+        binding.fullNameEt.onFocusChangeListener=this
+        binding.emailEt.onFocusChangeListener=this
+        binding.passwordEt.onFocusChangeListener=this
+        viewModel=ViewModelProvider(this,SignupActivityViewModelFactory(AuthRepository(APIService.getService()),application)).get(SignupActivityViewModel::class.java)
+        setupObservers()
     }
 
+    private fun setupObservers(){
+        viewModel.getIsLoading().observe(this){
+
+        }
+        viewModel.getErrorMessage().observe(this){
+
+        }
+        viewModel.getUser().observe(this){
+
+        }
+    }
     private fun validateFullName() : Boolean {
         var errorMessage: String? = null
-        val value: String = binding.fullName.text.toString()
+        val value: String = binding.fullNameEt.text.toString()
         if (value.isEmpty()) {
             errorMessage = "FullName required"
         }
+        if(errorMessage!=null){
+            binding.fullNameTil.apply {
+                isErrorEnabled=true
+                error=errorMessage
+            }
+        }
+
         return errorMessage==null
     }
     private fun validateEmail() : Boolean{
         var errorMessage : String? =null
-        val value: String=binding.editTextEmail.text.toString()
+        val value: String=binding.emailEt.text.toString()
         if(value.isEmpty()){
             errorMessage="Email required"
         }else if(Patterns.EMAIL_ADDRESS.matcher(value).matches()){
             errorMessage="Email address is invalid"
         }
+
+        if(errorMessage!=null){
+            binding.emailTil.apply {
+                isErrorEnabled=true
+                error=errorMessage
+            }
+        }
+
         return errorMessage == null
     }
 
     private fun validatePassword() : Boolean{
         var errorMessage : String? =null
-        val value: String=binding.editTextPassword.text.toString()
+        val value: String=binding.passwordEt.text.toString()
         if(value.isEmpty()){
             errorMessage="Password required"
         }else if(value.length<6){
             errorMessage="Password must be 6 characters long"
         }
+
+        if(errorMessage!=null){
+            binding.passwordTil.apply {
+                isErrorEnabled=true
+                error=errorMessage
+            }
+        }
+
         return errorMessage == null
     }
 
@@ -59,21 +101,34 @@ class SignupActivity : AppCompatActivity(), View.OnClickListener,View.OnFocusCha
         if(view!=null){
             when(view.id){
 
-                R.id.fullName ->{
+                R.id.fullNameTil ->{
                         if (hasFocus) {
-
+                        if (binding.fullNameTil.isErrorEnabled){
+                            binding.fullNameTil.isErrorEnabled=false
+                        }
                             }
                 else{validateFullName()}}
 
-                R.id.editTextEmail ->{
+                R.id.emailTil ->{
                     if (hasFocus) {
+                        if (binding.emailTil.isErrorEnabled){
+                            binding.emailTil.isErrorEnabled=false
+                        }
                     }
                     else{validateEmail()}}
 
-                R.id.editTextPassword ->{
+                R.id.passwordTil ->{
                     if (hasFocus) {
+                        if (binding.passwordTil.isErrorEnabled){
+                            binding.passwordTil.isErrorEnabled=false
+                        }
                     }
-                    else{validatePassword()}}
+                    else{
+                       validatePassword()
+
+
+
+                    }}
 
             }
         }
